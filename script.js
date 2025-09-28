d0fe1a51da2eb2a3835edab1e6fea80595a43a51
Juan Batista <juan.batista.dev@gmail.com>
2025-09-28 14:58:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,19 @@ document.querySelectorAll(".carousel").forEach(carousel => {
     showImage(index);
   });
 });
+// Seleciona todos os cards
+const cards = document.querySelectorAll('.atuacao-card');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.animationPlayState = 'running';
+      entry.target.classList.add('visible');
+    }
+  });
+}, { threshold: 0.2 }); // anima quando 20% do card estiver visível
+
+cards.forEach(card => {
+  card.style.animationPlayState = 'paused'; // pausa inicial
+  observer.observe(card);
+});
